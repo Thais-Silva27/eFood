eb5407493ffa44220c324757c4495ecedbea85a4
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-11-14 22:30:52 -0300

    Delete src directory

--- src/components/Listagem/index.tsx
+++
@@ -1,9 +0,0 @@
-import lista from '../../assets/images/lista.png'
-import { Card2 } from './styles'
-
-const Listagem = () => (
-  <Card2>
-    <img src={lista} alt="eFood" />
-  </Card2>
-)
-export default Listagem
