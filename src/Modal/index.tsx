2b3ff7cb54da33014154e3cb38fad6528e5c7760
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-11-27 22:24:37 -0300

    Add files via upload

---
+++ src/Modal/index.tsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export class Itens {
+  imagem: string
+  nome: string
+  descricao: string
+  porcao: string
+  preco: number
+
+  constructor(
+    imagem: string,
+    nome: string,
+    descricao: string,
+    porcao: string,
+    preco: number
+  ) {
+    this.imagem = imagem
+    this.nome = nome
+    this.descricao = descricao
+    this.porcao = porcao
+    this.preco = preco
+  }
+}
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { RodaPe } from './styles'
+
+import rodape from '../../assets/images/rodape.png'
+
+const Rodape = () => (
+  <RodaPe>
+    <img src={rodape} alt="Rodape" />
+  </RodaPe>
+)
+
+export default Rodape
