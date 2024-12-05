279ef36de8e73f7a95bcf609debedf0976a5f465
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-12-05 16:54:37 -0300

    Add files via upload

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const HeaderBar = styled.header`
+  background-color: ${cores.lightPink};
+  padding: 20px;
+  border-radius: 16px;
+  width: 1380px;
+  height: 186px;
+  margin-top: -12px;
+  margin-left: -36px;
+  margin-right: 10px;
+  margin-bottom: 32px;
+`
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,10 @@
+import { HeroBar } from './styles'
+import hero from '../../assets/images/hero.png'
+
+const Hero = () => (
+  <HeroBar>
+    <img src={hero} alt="eFood" />
+  </HeroBar>
+)
+
+export default Hero
