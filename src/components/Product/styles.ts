7a43a266321e964465b224211d965b1ceb3f8146
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-12-05 16:48:54 -0300

    Delete src directory

--- src/components/Product/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { cores } from '../../styles'
-
-export const Card = styled.div`
-  background-color: ${cores.lightPink};
-  padding-top: 50px;
-  margin-left: 171px;
-`
