566d5091e23426f7d7f029d1a2ae79cf93e23125
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-12-08 12:27:29 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { GlobalCSS } from './styles'
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalCSS /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
