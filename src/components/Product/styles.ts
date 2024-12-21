aecdd89a84b21a8ef468b26c9ab7146ec214eb63
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-12-21 15:29:42 -0300

    Add files via upload

---
+++ src/components/Product/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const Card = styled.div`
+  background-color: ${cores.lightPink};
+  padding-top: 50px;
+  margin-left: 5px;
+  margin-top: -50px;
+
+  img {
+    padding-left: 150px;
+    padding-bottom: 120px;
+    cursor: pointer;
+  }
+`
